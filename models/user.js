c09d4a9e14a1d2251a71e816785b77b03cf36a00
jgranger3 <jgranger3@ivytech.edu>
2024-10-01 18:20:27 -0400

    installed bcrypt to salt and hash passwords

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { isEmail} = require('validator');
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -22,8 +24,9 @@ userSchema.post('save', function (doc, next) {
 });
 
 //fire a function before doc save to db
-userSchema.pre('save', function(next){
-    console.log('user about to be created & save', this);
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
     next();
 })
 
