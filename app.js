7f3ee8e54b4fca12f1ddc58e99f94a495f3db508
jgranger3 <jgranger3@ivytech.edu>
2024-10-01 18:59:43 -0400

    worked on using cookies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -23,3 +24,25 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+//cookies
+app.get('/set-cookies', (req, res) => {
+
+  //res.setHeader('set-cookie', 'newUser=true');
+
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('you got the cookie!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+  
+  res.json(cookies);
+
+
+});
